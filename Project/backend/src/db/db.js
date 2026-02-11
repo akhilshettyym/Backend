7496df7451a895fd0c5057243a546c8508a912e8
Shetty, Akhil <AShetty3@globalindustrial.com>
2026-02-11 21:08:26 +0530

    Post and Play frontend and backend files updated

--- Project/backend/src/db/db.js
+++ Project/backend/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-    await mongoose.connect("mongodb uri/project");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to DB");
 }
 
