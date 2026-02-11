14805a776ec4aa83f0545d0961a14445ef3f6155
Shetty, Akhil <akhilshettym2003@gmail.com>
2026-02-11 22:24:18 +0530

    test update

--- Auth/src/db/db.js
+++ Auth/src/db/db.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 async function connectDB() {
 
     try {
+
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Database connected successfully");
+
     } catch (err) {
         console.error("Database connection error :", err);
     }
